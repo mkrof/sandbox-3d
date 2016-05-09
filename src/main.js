afcbeea6f9d8a389dfe065731547973c01a9e273
mkrof <mkrof@users.noreply.github.com>
2016-05-09 05:12:20 -0400

    Add shapes demo.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import redSphere from './red-sphere';
+import shapes from './shapes';
 
 window.addEventListener('DOMContentLoaded', function () {
   // redSphere(window.innerWidth, window.innerHeight);
+  shapes(window.innerWidth, window.innerHeight);
 });
